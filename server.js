2063053dd3eb2aed1d5b679f96df55171ff0af2c
Arthur <arthur.ferian@aluno.senai.br>
2025-08-22 09:30:56 -0300

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bruxos from "./src/data/bruxos.js"
+import bruxos from "./src/data/bruxos.js";
 
 const app = express();
 app.use(express.json());
@@ -7,43 +7,46 @@ app.use(express.json());
 const serverPort = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Servidor funcionando...")
-})
+  res.send("Servidor funcionando...");
+});
 
 app.get("/bruxos", (req, res) => {
-    res.json(bruxos);
-})
+  res.json(bruxos);
+});
 
 app.get("/bruxos/:id", (req, res) => {
-    const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id);
 
-    const bruxo = bruxos.find(b => b.id === id);
+  const bruxo = bruxos.find((b) => b.id === id);
 
-    if(bruxo){
-        res.status(200).json(bruxo);
-    } else {
-        res.status(404).json({
-            mensagem: "Bruxo(a) nao encontrado!"
-        })
-    }
-})
+  if (bruxo) {
+    res.status(200).json(bruxo);
+  } else {
+    res.status(404).json({
+      mensagem: "Bruxo(a) nao encontrado!",
+    });
+  }
+});
 
- app.get("/bruxos/nome/nome", (req, res) => {
-    let nome = req.params.nome;
-    nome = nome.toLowerCase();
- })
+app.get("/bruxos/nome/:nome", (req, res) => {
+  let nome = req.params.nome;
+  nome = nome.toLowerCase();
 
-    const nomesFiltrados = bruxos.filter(b => b.nome.toLowerCase().includes(nome));
+const nomesFiltrados = bruxos.filter((b) =>
+  b.nome.toLowerCase().includes(nome)
+);
 
-    if (nomesFiltrados) {
-        res.status(200).json(nomesFiltrados);
-    } else {
-        res.status(404).json({
-            mensagem: "Bruxos não encontrado!"
-        })
-    }
+if (nomesFiltrados) {
+  res.status(200).json(nomesFiltrados);
+} else {
+  res.status(404).json({
+    mensagem: "Bruxos não encontrado!",
+  });
+}
+});
+
 
 
 app.listen(serverPort, () => {
-    console.log("Servidor esta rodando...");
+  console.log("Servidor esta rodando...");
 });
