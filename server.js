36892c7823efb38179c3e3e76c7595398c6717ac
Arthur <arthur.ferian@aluno.senai.br>
2025-08-22 09:05:27 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bruxos from "./src/data/bruxos.js"
+
+const app = express();
+app.use(express.json());
+
+const serverPort = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Servidor funcionando...")
+})
+
+app.get("/bruxos", (req, res) => {
+    res.json(bruxos);
+})
+
+app.get("/bruxos/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const bruxo = bruxos.find(b => b.id === id);
+
+    if(bruxo){
+        res.status(200).json(bruxo);
+    } else {
+        res.status(404).json({
+            mensagem: "Bruxo(a) nao encontrado!"
+        })
+    }
+})
+
+
+app.listen(serverPort, () => {
+    console.log("Servidor esta rodando...");
+});
